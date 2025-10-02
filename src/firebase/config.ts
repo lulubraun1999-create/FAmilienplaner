6c8daec02622ec7260ec3ff592aee92eebaeff4b
FIstNCISBall <lulubraun1999@gmail.com>
2025-10-02 02:20:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1316048977-de88c",
+  "appId": "1:348135007607:web:bf5d691700aef508db6d72",
+  "apiKey": "AIzaSyBydSBefU8d1Wei9fZa9YIgZ8EjYvBDKfc",
+  "authDomain": "studio-1316048977-de88c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "348135007607"
+};
